9a0e8dd7aec7569e506aa24259b7a9cfa636af97
Quico Orozco <esi@plumbaria.com>
2019-02-26 18:48:51 +0100

    Commit inicial Ejercicio 10.10. Leer datos de la base de datos de Firebase.

---
+++ app/src/main/java/com/plumbaria/e_10_10_ejemplofirebase2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.plumbaria.e_10_10_ejemplofirebase2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
